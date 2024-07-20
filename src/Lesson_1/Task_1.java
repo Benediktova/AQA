b056fae831de993e9207e50517886a2c23007cad
Tatiana B <zametka.yar@mail.ru>
2024-07-20 22:57:29 +0300

    Выкладываю ДЗ по Lesson_1

---
+++ src/Lesson_1/Task_1.java
@@ -0,0 +1,14 @@
+package Lesson_1;
+
+public class Task_1 {
+        /*
+         1. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова:
+         Orange, Banana, Apple
+         */
+        //public static void main(String[] args){
+        public void printThreeWords() {
+                System.out.println("Orange");
+                System.out.println("Banana");
+                System.out.println("Apple");
+            }
+}
---
+++ src/Lesson_1/Task_3.java
@@ -0,0 +1,22 @@
+package Lesson_1;
+
+public class Task_3 {
+    /*
+    3. Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте
+    ее любым значением.
+    Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”,
+    если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”,
+    если больше 100 (100 исключительно) - “Зеленый”;
+     */
+    //public static void main(String[] args){
+    public void printColor() {
+    int value = 0;
+    if (value <= 0) {
+        System.out.println("Красный");
+    } else if (value > 0 && value <= 100) {
+        System.out.println("Желтый");
+    } else if (value > 100) {
+        System.out.println("̆Зеленый");
+    }
+    }
+}
