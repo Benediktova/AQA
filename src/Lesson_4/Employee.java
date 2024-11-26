5d837a6fda45dc9776ea2f6905508f43f9661dee
Tatiana B <zametka.yar@mail.ru>
2024-11-26 12:23:23 +0300

    исправление к лекции 4

--- src/Lesson_4/Employee.java
+++ src/Lesson_4/Employee.java
@@ -1,20 +1,19 @@
+package Lesson_4;
 /*
 Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
 
 Конструктор класса должен заполнять эти поля при создании объекта.
 Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.
  */
-
-package Lesson_4;
 public class Employee {
     private String Name;
     private String Position;
     private String Email;
     private String PhoneNumber;
-    private Integer Salary;
-    Integer Age;
+    private int Salary;
+    public int Age;
 
-    public Employee(String Name, String Position, String Email, String PhoneNumber, Integer Salary, Integer Age) {
+    public Employee(String Name, String Position, String Email, String PhoneNumber, int Salary, int Age) {
         this.Name = Name;
         this.Position = Position;
         this.Email = Email;
@@ -22,12 +21,13 @@ public class Employee {
         this.Salary = Salary;
         this.Age = Age;
     }
-    public void displayInformation () {
-        System.out.println("Name: " + Name);
-        System.out.println("Position: " + Position);
-        System.out.println("Email: " + Email);
-        System.out.println("PhoneNumber: " + PhoneNumber);
-        System.out.println("Salary: " + Salary);
-        System.out.println("Age: " + Age);
+
+    public void displayInformation() {
+        System.out.println("Name: " + this.Name);
+        System.out.println("Position: " + this.Position);
+        System.out.println("Email: " + this.Email);
+        System.out.println("PhoneNumber: " + this.PhoneNumber);
+        System.out.println("Salary: " + this.Salary);
+        System.out.println("Age: " + this.Age);
     }
 }
--- src/Lesson_4/Lesson_4.java
+++ src/Lesson_4/Lesson_4.java
@@ -1,3 +1,4 @@
+package Lesson_4;
 /*Создать массив из 5 сотрудников.
 Пример:
 // вначале объявляем массив объектов
@@ -10,21 +11,26 @@ persArray[1] = new Person(...);
 persArray[4] = new Person(...);
 С помощью цикла вывести информацию только о сотрудниках старше 40 лет.
 */
-package Lesson_4;
-
 public class Lesson_4 {
-    public static void main(String[] args) {
-        Employee[] employeesArray = new Employee[5];
-        employeesArray[0] = new Employee("Belyakova TO", "QA", "Belyakova@gmail.com", "+79541214568", 100000, 40);
-        employeesArray[1] = new Employee("Ivanov EL", "QC", "Ivanov@gmail.com", "+79216547896", 110000, 39);
-        employeesArray[2] = new Employee("Petrov PT", "TO", "Petrov@gmail.com", "+79541236574", 120000, 45);
-        employeesArray[3] = new Employee("Sidorov IA", "BA", "Sidorov@gmail.com", "+7954748596", 130000, 56);
-        employeesArray[4] = new Employee("Stepanova AC", "Dev", "Stepanova@gmail.com", "+79201247586", 140000, 27);
+    public Lesson_4() {
+    }
 
-              for (Employee employee : employeesArray) {
-                if (employee.Age > 40) {
-                    employee.displayInformation();
-                }
+    public static void main(String[] args) {
+        Employee[] employeesArray = new Employee[]{
+                new Employee("Belyakova TO", "QA", "Belyakova@gmail.com", "+79541214568", 100000, 40),
+                new Employee("Ivanov EL", "QC", "Ivanov@gmail.com", "+79216547896", 110000, 39),
+                new Employee("Petrov PT", "TO", "Petrov@gmail.com", "+79541236574", 120000, 45),
+                new Employee("Sidorov IA", "BA", "Sidorov@gmail.com", "+7954748596", 130000, 56),
+                new Employee("Stepanova AC", "Dev", "Stepanova@gmail.com", "+79201247586", 140000, 27)
+        };
+        Employee[] var2 = employeesArray;
+        int var3 = employeesArray.length;
+
+        for(int var4 = 0; var4 < var3; ++var4) {
+            Employee employee = var2[var4];
+            if (employee.Age > 40) {
+                employee.displayInformation();
             }
         }
+    }
 }
